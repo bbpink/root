3782372777879b6161b4f2a3a34e8771bd3e9741
bbpink <bbpink@sevensenses.jp>
2020-07-01 13:36:20 +0900

    feat: update for v1.1.0

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
   sevensenses.jp root web site ( https://sevensenses.jp )
 
 ```
-sudo docker build -t sevensenses/root ./docker/
+sudo docker build -t sevensenses/root -f docker/Dockerfile .
 sudo docker tag IMAGE_ID docker.pkg.github.com/bbpink/root/root:vX.Y.Z
 sudo docker push docker.pkg.github.com/bbpink/root/root:vX.Y.Z
 ```
--- public/index.js
+++ public/index.js
@@ -1,38 +1,40 @@
-$(document).ready(function(){
-    //195x195
-    var c = $("#heptagon")[0].getContext('2d');
-    c.fillStyle = "#ffffff";
-    c.strokeStyle = "#000000";
-    c.lineWidth = 2;
-    var cx = 97.5;
-    var cy = 97.5;
-    var size = 65;
-    var px = 0;
-    var py = 0;
+document.addEventListener("DOMContentLoaded", function() {
 
-    c.beginPath();
-    var a = 180 + 360 / 7;
-    var r = a * Math.PI / 180;
-    c.moveTo(cx+(Math.sin(r)*size), cy+(Math.cos(r)*size));
+  // 195x195
+  const c = document.querySelector("#heptagon").getContext("2d");
+  c.fillStyle = "#ffffff";
+  c.strokeStyle = "#000000";
+  c.lineWidth = 2;
+  const cx = 97.5;
+  const cy = 97.5;
+  const size = 65;
+  var px = 0;
+  var py = 0;
 
-    for (var i=0; i <= 7; i++) {
-	var x = cx;
-	var y = cy;
-	var angle = 180 + 360 / 7 * i;
-	var rad = angle * Math.PI / 180;
+  c.beginPath();
+  const a = 180 + 360 / 7;
+  const r = a * Math.PI / 180;
+  c.moveTo(cx+(Math.sin(r)*size), cy+(Math.cos(r)*size));
 
-	x += Math.sin(rad) * size;
-	y += Math.cos(rad) * size;
+  for (var i=0; i <= 7; i++) {
+    var x = cx;
+    var y = cy;
+    const angle = 180 + 360 / 7 * i;
+    const rad = angle * Math.PI / 180;
 
-	if (angle != 180) {
-	    c.lineTo(x,y);
-	}
+    x += Math.sin(rad) * size;
+    y += Math.cos(rad) * size;
 
-	px = x;
-	py = y;
+    if (angle != 180) {
+      c.lineTo(x,y);
     }
 
-    c.closePath();
-    c.stroke();
-    c.fill();
+    px = x;
+    py = y;
+  }
+
+  c.closePath();
+  c.stroke();
+  c.fill();
+
 });
